af820bf6052b55651182f76d6feeed6ebcebcf95
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-10-29 22:40:39 -0700

    Added simple get request and narrowed down the HTML code to the container containing the charts with the data I need.

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ app.get('/', (req, res) => {
 
 app.get('/scrape', (req, res) => {
   let url = 'https://www.cnbc.com/pre-markets/';
-  
+  // Get CNBC Premarket Webpage HTML Document
+  axios.get(url)
+  .then((response) => {
+    // Store retrieved HTML data
+    let html = response.data;
+    // Parse and Scrape using Cheerio
+    const $ = cheerio.load(html);
+    console.log($('.unit').attr('itemprop', 'mainContentOfPage').html());
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 });
 
 app.listen(port, () => console.log(`CNBC Premarket Webscraper is listening on Port ${port}`));
